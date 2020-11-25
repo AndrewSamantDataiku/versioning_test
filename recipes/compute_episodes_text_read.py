3d2b88f84d70403cf6de8c43efc9d13b4c76fb21
admin <admin>
2020-11-25 21:49:34 +0000

    Saved recipe: PODCAST_READER.compute_episodes_text_read

--- recipes/compute_episodes_text_read.py
+++ recipes/compute_episodes_text_read.py
@@ -44,6 +44,7 @@ def read_episode(url,length):
                              "-r","16000",
                              "-ac","1", 
                              "-t","30",
+                             "--probesize","5000000000",
                              wav_path])
         
         
