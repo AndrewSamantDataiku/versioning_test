d399e6f56569240d2fd30eeefb02be976ed48b84
admin <admin>
2020-12-07 18:12:39 +0000

    Saved recipe: PODCAST_READER.compute_episodes_text_read

--- recipes/compute_episodes_text_read.py
+++ recipes/compute_episodes_text_read.py
@@ -86,7 +86,7 @@ def read_episode(url,length):
 episodes_read = dataiku.Dataset("episodes_text_read")
 
 episodes_sample_df['text'] = ''
-size = 100
+size = 2
 for i in range(0, len(episodes_sample_df),size):
     write_df = episodes_sample_df.loc[i:i+size-1,:]
     write_df['text'] = write_df.apply(lambda row: read_episode(row['audio_url'],row['length']), axis=1)
