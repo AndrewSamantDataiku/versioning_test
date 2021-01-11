a6ef1f015d9f5555f7ce6472337be9742b45f72f
admin <admin>
2021-01-11 17:33:15 +0000

    Saved recipe: PODCAST_READER.compute_klPrZDXv

--- recipes/compute_klPrZDXv.py
+++ recipes/compute_klPrZDXv.py
@@ -17,7 +17,7 @@ wav_folder_path = wav_folder.get_path()
 
 ffmpeg_path = '/data/dataiku/data_dir/code-envs/python/ffmpeg_27/bin/ffmpeg/ffmpeg-4.3.1-amd64-static/ffmpeg'
 
-for mp3 in mp3_folder.list_paths_in_partition()[0:2]:
+for mp3 in mp3_folder.list_paths_in_partition():
     print(mp3_folder_path + mp3)
     print(wav_folder_path + mp3[:-4]+ "_part_%03d.wav")
     subprocess.call([ffmpeg_path,"-y",
