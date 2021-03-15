4c9bfc4ca5f5ac2c462b2fab68e00c7474e3bf87
admin <admin>
2021-03-15 22:46:48 +0000

    Recipe PODCAST_READER.compute_wav_files_ds saved through notebook notebook editor for compute_wav_files_ds

--- recipes/compute_wav_files_ds.py
+++ recipes/compute_wav_files_ds.py
@@ -1,18 +1,27 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import os
 
 # Read recipe inputs
 wav_files_local = dataiku.Folder("klPrZDXv")
-wav_files_local_info = wav_files_local.get_info()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+wav_files = wav_files_local.list_paths_in_partition()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+folder_path = wav_files_local.get_path()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+wav_paths = [os.path.join([folder_path,f[1:]]) for f in wav_files]
+wav_files = [f[1:-4] for f in wav_files]
 
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-wav_files_ds_df = ... # Compute a Pandas dataframe to write into wav_files_ds
+
+wav_files_ds_df = pd.DataFrame({"file":wav_files,"path":wav_paths})
 
 
 # Write recipe outputs
